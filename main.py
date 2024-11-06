78de411eddd8ca0ea5fb28b8ce67abfd3d7d190f
Anuraganand Sharma <anuraganand.sharma@gmail.com>
2024-11-07 08:10:45 +1200

    Update main.py
    still first version

--- main.py
+++ main.py
@@ -4,15 +4,24 @@ from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 from model import CNN_GSGD, GSGDOptimizer
 from train import train, test
+import os
 
 # Data loading, model setup, and main training loop code here
+# Define the path where the data should be stored
+data_path = './data'
+
+# Check if the data directory exists
+download_data = not os.path.exists(os.path.join(data_path, 'MNIST'))
+
+# Define the transform
 transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.1307,), (0.3081,))
 ])
 
-train_dataset = datasets.MNIST('./data', train=True, download=True, transform=transform)
-test_dataset = datasets.MNIST('./data', train=False, transform=transform)
+# Load the datasets, only downloading if necessary
+train_dataset = datasets.MNIST(data_path, train=True, download=download_data, transform=transform)
+test_dataset = datasets.MNIST(data_path, train=False, download=download_data, transform=transform)
 
 train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=1000, shuffle=False)
